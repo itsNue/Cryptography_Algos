fb6d3688653414c80652fba8b5d0a3283e009dfa
Uvindu Harshana <uvinduharshana@zohomail.com>
2022-06-23 09:08:27 +0530

    restructed the files

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//AES
 function encrypt() {
   let encrypted = CryptoJS.AES.encrypt(
     document.getElementById("text").value,
@@ -16,3 +17,23 @@ function decrypt() {
   document.getElementById("decrypted").innerHTML = decrypted;
   document.getElementById("encryptedValue").innerHTML = "";
 }
+//RSA
+//tab switch
+function onTabClick(event) {
+  let activeTabs = document.querySelectorAll(".active");
+
+  // deactivate existing active tab and panel
+
+  activeTabs.forEach(function (tab) {
+    tab.className = tab.className.replace("active", "");
+  });
+
+  // activate new tab and panel
+  event.target.parentElement.className += " active";
+  document.getElementById(event.target.href.split("#")[1]).className +=
+    " active";
+}
+
+const element = document.getElementById("nav-tab");
+
+element.addEventListener("click", onTabClick, false);
